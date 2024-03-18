83391d9130c8e4de496a9f8d575e9222a3245af5
Yashbabani0 <yashbabani09@gmail.com>
2024-03-18 17:16:31 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+
+// Selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// Strting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+let currentScore = 0;
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  // 1. Generatng a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch t next player
+});
